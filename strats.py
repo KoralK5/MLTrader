696345a4c122308dc3c39542ff8a023e910fcd6e
Koral Kulacoglu <kulacoglukoral@gmail.com>
2023-04-07 16:52:05 -0400

    fixed minor stuff

--- strats.py
+++ strats.py
@@ -3,7 +3,7 @@ from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 
 def stopLoss(client, symbol, cash, stop=0.95, take=1.05):
-    symbol_bars = api.get_barset(symbol, 'minute', 1).df.iloc[0]
+    symbol_bars = client.get_barset(symbol, 'minute', 1).df.iloc[0]
     symbol_price = symbol_bars[symbol]['close']
 
     client.submit_order(order_data=MarketOrderRequest(
