960e74a7c44d054b2288b349860c11aae668c782
Koral Kulacoglu <kulacoglukoral@gmail.com>
2023-04-07 22:17:27 -0400

    fix

--- bot.py
+++ bot.py
@@ -14,14 +14,14 @@ SECRET_KEY = 'eElymeK26x5bKKn5yuPmUZz2SOXVLeg14LEnLLo2'
 client = TradingClient(API_KEY, SECRET_KEY, paper=True)
 
 df = pd.DataFrame(si.tickers_sp500())
-symbols = list(set(symbol for symbol in df[0].values.tolist()))[:20]
+symbols = set(symbol for symbol in df[0].values.tolist())
 
 results = sentiments(symbols, progress=True)
 ordSents = dict(sorted(results.items(), key=lambda item: item[1], reverse=True))
 
-cash = 100
+cash = 1000
 
 for stockName in list(ordSents)[:20]:
-    print(f'Buying 100$ of {stockName}')
+    print(f'Buying ${cash} of {stockName}')
     stopLoss(client, stockName, cash, stop=0.95, take=1.05)
 
