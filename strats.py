e75722ca335b514f2dbefff519f6b3debe8c06ba
Koral Kulacoglu <kulacoglukoral@gmail.com>
2023-04-14 13:49:59 -0400

    errors

--- strats.py
+++ strats.py
@@ -1,16 +1,9 @@
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
-import numpy as np
 
 import requests
 from bs4 import BeautifulSoup
-
-def ceil(a, precision=0):
-    return np.true_divide(np.ceil(a * 10**precision), 10**precision)
-
-def floor(a, precision=0):
-    return np.true_divide(np.floor(a * 10**precision), 10**precision)
 
 def getPrice(symbol):
     url = f"https://finance.yahoo.com/quote/{symbol}"
@@ -23,8 +16,8 @@ def getPrice(symbol):
 
 def stopLoss(client, symbol, cash, buy=True, stop=0.95, take=1.05):
     symbol_price = getPrice(symbol)
-    stop_loss = floor(symbol_price * stop, 2) if buy else ceil(symbol_price * stop, 2)
-    take_profit = ceil(symbol_price * take, 2) if buy else floor(symbol_price * take, 2)
+    stop_loss = min(symbol_price * stop, symbol_price - 0.01) if buy else max(symbol_price * stop, symbol_price + 0.01)
+    take_profit = max(symbol_price * take, symbol_price + 0.01) if buy else min(symbol_price * take, symbol_price - 0.01)
 
     client.submit_order(order_data=MarketOrderRequest(
         symbol=symbol,
@@ -33,6 +26,6 @@ def stopLoss(client, symbol, cash, buy=True, stop=0.95, take=1.05):
         type='market',
         time_in_force='day',
         order_class='bracket',
-        stop_loss={'stop_price': stop_loss},
-        take_profit={'limit_price': take_profit}
+        stop_loss={'stop_price': round(stop_loss, 2)},
+        take_profit={'limit_price': round(take_profit, 2)}
     ))
